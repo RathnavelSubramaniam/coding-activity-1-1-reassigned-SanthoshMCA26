7b88098acfc42d8f9da94902f6014d4104f1f913
Santhosh M <santhoshrvs26@gmail.com>
2026-08-17 21:47:20 +0530

    Add files via upload
    completed activity-1.1

---
+++ variable01.js
@@ -0,0 +1,19 @@
+function greetings() {
+  let myFriend = "Santhosh";
+  return "Greetings " + myFriend + ".";
+}
+
+console.log("results: ", greetings());
+
+let currentYear = 2026; 
+console.log("current year result: ", currentYear);
+
+const FruitName = "grapefruit"; 
+function testBestFlavor() {
+  const FruitColor = "orange"; 
+  return `Ana says, FruitName ${FruitColor} is a better flavor than FruitName ${FruitName}.`;
+}
+
+console.log(testBestFlavor());
+
+
